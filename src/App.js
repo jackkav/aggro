667fc3073d6db66ac31fb2c30a0d000772532b2a
Jack Kavanagh <jackkav@gmail.com>
2017-11-15 18:11:25 +0800

    caching json in localstorage :children_crossing:

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react'
 import cheerio from 'cheerio'
-import Grid from 'react-css-grid'
 import styled from 'styled-components'
-import logo from './logo.svg'
-import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        {/* <Ez /> */}
-        {/* <Pb /> */}
-      </div>
+      <Root>
+        {/* <Column>
+          <Heading>desc</Heading>123123123123123
+        </Column>
+        <Column>
+          <Heading>desc</Heading>
+          <Pb />
+        </Column>
+        <Column>
+          <Heading>desc</Heading>
+          <Ez />
+        </Column> */}
+        <Pb />
+      </Root>
     )
   }
 }
-const Box = styled.div`
+const Root = styled.div`
+  display: flex;
+  flex: 1;
+`
+const Column = styled.div`
+  flex: 1;
   background-color: #444;
   color: #fff;
-  border-radius: 5;
+  padding: 20;
+`
+const Heading = styled.div`
+  flex: 1;
+  background-color: #444;
+  color: #fff;
   padding: 20;
 `
 export default App
@@ -33,29 +46,37 @@ class Pb extends Component {
     loading: false,
   }
   componentDidMount() {
-    this.setState({ loading: true })
-    fetch('https://cors-anywhere.herokuapp.com/thepiratebay.org/top/205')
-      .then(resp => resp.text())
-      .then(body => {
-        const $ = cheerio.load(body)
-        $('a[title="Download this torrent using magnet"]').each((a, item) => {
-          const magnet = item.attribs.href
-          const name = $(item)
-            .parent()
-            .text()
-          this.setState({ shows: [...this.state.shows, { name, magnet }] })
-          // this.setState({ shows: [{ name, magnet }] })
-          this.setState({ loading: false })
+    //TODO: invalidate cache after one hour
+    if (localStorage.getItem('aggro.pb')) {
+      console.log('loading cached scrape')
+      this.setState({ shows: JSON.parse(localStorage.getItem('aggro.pb')) })
+    } else {
+      localStorage.setItem('aggro.updated', JSON.stringify(new Date()))
+      console.log('loading fresh scrape')
+      this.setState({ loading: true })
+      fetch('https://cors-anywhere.herokuapp.com/thepiratebay.org/top/205')
+        .then(resp => resp.text())
+        .then(body => {
+          const $ = cheerio.load(body)
+          $('a[title="Download this torrent using magnet"]').each((a, item) => {
+            const magnet = item.attribs.href
+            const name = $(item)
+              .parent()
+              .text()
+            this.setState({ shows: [...this.state.shows, { name, magnet }] })
+            // this.setState({ shows: [{ name, magnet }] })
+            this.setState({ loading: false })
+          })
+          localStorage.setItem('aggro.pb', JSON.stringify(this.state.shows))
         })
-      })
-      .catch(function(error) {
-        console.log(JSON.stringify(error))
-      })
+        .catch(function(error) {
+          console.log(JSON.stringify(error))
+        })
+    }
   }
   render() {
     return (
       <div>
-        <h1>Authors</h1>
         {this.state.loading && <div>loading</div>}
         <ul id="authors" style={{ listStyleType: 'none', textAlign: 'left' }}>
           {this.state.shows.map((x, i) => (
@@ -121,12 +142,14 @@ class Ez extends Component {
   render() {
     return (
       <div>
-        <h1>Authors</h1>
         {this.state.loading && <div>loading</div>}
         <ul id="authors" style={{ listStyleType: 'none', textAlign: 'left' }}>
           {this.state.shows.map((x, i) => (
             <li key={i}>
-              <a href={x.magnet}>{x.name}</a>
+              <a href={x.magnet}>
+                <img src="https://eztv.ag/images/magnet-icon-5.png" />
+              </a>
+              {x.name}
             </li>
           ))}
         </ul>
