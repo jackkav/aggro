5b42b3469fd19229e8058400a7a9267c6d29045f
Jack Kavanagh <jackkav@gmail.com>
2018-01-16 22:07:49 +0800

    skeleton title and poster

--- src/components/Movie/MovieCard.js
+++ src/components/Movie/MovieCard.js
@@ -1,7 +1,73 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+const Title = props => (
+  <div>
+    {props.movie.title ? (
+      <h4 className="card-title">{props.movie.title}</h4>
+    ) : (
+      <h4
+        className="card-title"
+        style={{
+          background: 'gray',
+          minWidth: 50,
+          minHeight: 50,
+        }}
+      />
+    )}
+  </div>
+)
+const Image = props => (
+  <div>
+    {props.movie.imageUrl ? (
+      <img className="card-img-top" src={props.movie.imageUrl} alt="" />
+    ) : (
+      <div
+        className="card-img-top"
+        style={{
+          background: 'gray',
+        }}
+      />
+    )}
+  </div>
+)
 const MovieCard = props => (
+  <div className="movie-card">
+    <div className="movie-card card">
+      <Image movie={props.movie} />
+      <div className="card-body">
+        <Title movie={props.movie} />
+        <h6 className="card-subtitle mb-2 text-muted">
+          {props.movie.subtitle}
+        </h6>
+        <p className="text-justify" style={{ fontSize: '14px' }}>
+          {props.movie.description}
+        </p>
+      </div>
+      <div className="card-footer">
+        <div className="clearfix">
+          <div className="float-left">
+            <img
+              src="http://icons.iconarchive.com/icons/dtafalonso/android-lollipop/96/Youtube-icon.png"
+              style={{
+                height: 30,
+              }}
+            />
+            <img
+              src="http://icons.iconarchive.com/icons/emey87/trainee/16/Magnet-icon.png"
+              style={{
+                height: 30,
+              }}
+            />
+          </div>
+          <div className="card-footer-badge float-right badge badge-primary badge-pill">
+            {props.movie.rating || 'loading'}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+const MovieCardOld = props => (
   <div className="movie-card">
     <div className="movie-card card">
       <img className="card-img-top" src={props.movie.imageUrl} alt="" />
@@ -38,7 +104,6 @@ const MovieCard = props => (
     </div>
   </div>
 )
-
 MovieCard.defaultProps = {
   movie: {},
 }
